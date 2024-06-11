cd8331de4bd5480fb342bda3fd8f17af5c857632
Ezra <dulaneyezra@gmail.com>
2024-06-11 16:21:47 -0400

    fixes bugs

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,7 +9,22 @@ function addNewItem() {
     } else {
         localStorage.setItem(itemText.toString(), itemText.toString())
         console.log(itemText);
-        form.insertAdjacentHTML("beforeend", `<div class="list-item"><label><input type="checkbox">${itemText}</label></div>`);
+        form.insertAdjacentHTML("beforeend", `<div class="list-item"><label onclick="removeItem(event)"><input type="checkbox">${itemText}</label></div>`);
         userInput.value = "";
     }
 }
+
+function removeItem(element) {
+    let itemToRemove = element.target.labels[0].innerText.toString();
+    if(itemToRemove in localStorage) {
+        localStorage.removeItem(itemToRemove)
+    } else {
+        localStorage.setItem(itemToRemove, itemToRemove)
+    }
+}
+
+window.addEventListener("load", (event) => {
+    for (const [key, value] of Object.entries(localStorage)) {
+        form.insertAdjacentHTML("beforeend", `<div class="list-item"><label onclick="removeItem(event)"><input type="checkbox">${value}</label></div>`);
+      }
+  });
--- assets/js/index2.js
+++ assets/js/index2.js
@@ -2,11 +2,38 @@
 
 function addingItemFunc() {
     let li = document.createElement("li");
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
     let newShoppingItem = document.getElementById("shoppingInput");
-    let textnode = document.createTextNode(newShoppingItem.value);
-    li.appendChild(textnode);
+    let span = document.createElement("span");
+    span.textContent = newShoppingItem.value;
+    li.appendChild(checkbox);
+    li.appendChild(span);
     document.getElementById("listItems").appendChild(li);
     document.getElementById("shoppingInput").value = "";
+    
+    //event Listener for the checkbox
+    checkbox.addEventListener('change', function() {
+        if (this.checked) {
+            span.style.textDecoration = "line-through";
+            span.style.textDecoration.color = "red";
+        } else {
+            span.style.textDecoration = "none";
+        }
+    });
+    // let li = document.createElement("li");
+    // let checkbox = document.createElement("input")
+    // checkbox.type = "checkbox";
+    // let newShoppingItem = document.getElementById("shoppingInput");
+    // let textnode = document.createTextNode(newShoppingItem.value);
+    // li.appendChild(checkbox);
+    // li.appendChild(textnode);
+    // document.getElementById("listItems").appendChild(li);
+    // document.getElementById("shoppingInput").value = "";
 
-
+    // checkbox.addEventListener('change', function() {
+    //     if(this.checked) {
+    //         span
+    //     }
+    // })
 }
